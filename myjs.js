fa409a8fb690480f818d5acae5c3857b5adec2f9
Andy Kwiatkowski <andy.kwiatkowski@shopify.com>
2020-03-25 16:46:17 -0400

    caching coordinates

--- myjs.js
+++ myjs.js
@@ -19,9 +19,83 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1IjoiYWt3aWF0IiwiYSI6ImNrODdqOGlqejAwanMzZm5tMTBvMmp5NmoifQ.igfG4pPw8BufL5PKy1BIkA'
 }).addTo(mymap);
 
+async function getCoords(place) {
+    var value = getCookie("coords"+ place).split(",");
+    if (value && value != "" ){
+        return value;
+    }
+
+    var addr = encodeURIComponent(place + ",guelph, ontario");
+    var url3 = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + addr + ".json?access_token=pk.eyJ1IjoiYWt3aWF0IiwiYSI6ImNrODdqOGlqejAwanMzZm5tMTBvMmp5NmoifQ.igfG4pPw8BufL5PKy1BIkA"
 
 
-var url="https://api.mapbox.com/directions/v5/mapbox/driving?access_token=pk.eyJ1IjoiYWt3aWF0IiwiYSI6ImNrODdqOGlqejAwanMzZm5tMTBvMmp5NmoifQ.igfG4pPw8BufL5PKy1BIkA"
+    return fetch(url3, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+    }).then((response) => {
+        return response.json();
+    })
+        .then((data) => {
+            console.log(data);
+            let coords = data.features[0].center.reverse();
+            setCookie("coords"+place,coords,30);
+            return coords;});
+/*            L.marker(coords).addTo(mymap);
+
+            var popup = L.popup()
+                .setLatLng(latlng)
+                .setContent('<p>Hello world!<br />This is a nice popup.</p>')
+                .openOn(mymap);
+
+
+        });*/
+}
+let places = ["the bookshelf", "79 Dean Ave.", "Second Cup", "Angel's Diner",
+"15 Boulder Cr.", "Canadian Tire", "Walmart","Molloy's"];
+places.forEach((item)=>{
+    getCoords(item).then((coords)=>{
+        if (coords == null ){
+            console.log("Can't get item " + item);
+            return;
+        }
+
+        L.marker(coords).addTo(mymap);
+    });
+});
+
+
+
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+
+
+
+/*var url="https://api.mapbox.com/directions/v5/mapbox/driving?access_token=pk.eyJ1IjoiYWt3aWF0IiwiYSI6ImNrODdqOGlqejAwanMzZm5tMTBvMmp5NmoifQ.igfG4pPw8BufL5PKy1BIkA"
 fetch(url, {
     method: 'POST',
     headers: {
@@ -34,26 +108,4 @@ fetch(url, {
 })
     .then((data) => {
         console.log(data);
-    });
-var feature;
-var addr = encodeURIComponent("the bookshelf, guelph, ontario");
-var url3 ="https://api.mapbox.com/geocoding/v5/mapbox.places/"+addr+".json?access_token=pk.eyJ1IjoiYWt3aWF0IiwiYSI6ImNrODdqOGlqejAwanMzZm5tMTBvMmp5NmoifQ.igfG4pPw8BufL5PKy1BIkA"
-//var url2="https://api.mapbox.com/geocoding/v5/mapbox.places/guelph.json?access_token=pk.eyJ1IjoiYWt3aWF0IiwiYSI6ImNrODdqOGlqejAwanMzZm5tMTBvMmp5NmoifQ.igfG4pPw8BufL5PKy1BIkA"
-fetch(url3, {
-    method: 'GET',
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/x-www-form-urlencoded',
-    }
-   // body:"coordinates=-80.2482,43.5448;-80.2462,43.5448&&steps=true"
-}).then((response) => {
-    return response.json();
-})
-    .then((data) => {
-        feature=data;
-        console.log(data);
-        let coords =feature.features[0].center.reverse();
-        L.marker(coords).addTo(mymap);
-
-    });
-
+    });*/
