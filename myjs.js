3b9b533362ca43d4637e6ba83f467d9f7356f65c
Andy Kwiatkowski <andy.kwiatkowski@shopify.com>
2020-03-25 17:31:56 -0400

    distances working

--- myjs.js
+++ myjs.js
@@ -53,9 +53,10 @@ async function getCoords(place) {
 
         });*/
 }
-let places = ["the bookshelf", "79 Dean Ave.", "Second Cup", "Angel's Diner",
-"15 Boulder Cr.", "Canadian Tire", "Walmart","Molloy's"];
-places.forEach((item)=>{
+let places=[];
+let placeNames = ["15 Boulder Cr.","79 Dean Ave.","the bookshelf",  "Second Cup", "Angel's Diner",
+ "Canadian Tire", "Walmart","Molloy's"];
+placeNames.forEach((item)=>{
     getCoords(item).then((coords)=>{
         if (coords == null ){
             console.log("Can't get item " + item);
@@ -63,6 +64,11 @@ places.forEach((item)=>{
         }
 
         L.marker(coords).addTo(mymap);
+        places.push({name:item, coords:coords });
+        if (places.length >= placeNames.length){
+            onEverythingLoaded();
+        }
+
     });
 });
 
@@ -94,6 +100,7 @@ function getCookie(cname) {
 
 
 
+// ROUTING /DIRECTIONS
 
 /*var url="https://api.mapbox.com/directions/v5/mapbox/driving?access_token=pk.eyJ1IjoiYWt3aWF0IiwiYSI6ImNrODdqOGlqejAwanMzZm5tMTBvMmp5NmoifQ.igfG4pPw8BufL5PKy1BIkA"
 fetch(url, {
@@ -109,3 +116,26 @@ fetch(url, {
     .then((data) => {
         console.log(data);
     });*/
+
+let distances={};
+function calcDistance(i1, i2 ){
+    let x1 = parseFloat(places[i1].coords[0]);
+    let x2 = parseFloat(places[i2].coords[0]);
+    let y1 = parseFloat(places[i1].coords[1]);
+    let y2 = parseFloat(places[i2].coords[1]);
+    let dx = x2-x1;
+    let dy = y2-y1;
+    return Math.sqrt(dx*dx+dy*dy)*100;// estimate
+}
+function getDistance( i1, i2 ){
+    let idx =  i1 + "->" + i2;
+    if ( !distances[idx] ){
+        distances[idx] = calcDistance(i1,i2);
+    }
+    return distances[idx];
+}
+function onEverythingLoaded(){
+    console.log("OK");
+    console.log("boulder to booksehlf:" + getDistance( 0,2) );
+    console.log("dean to bookshelf:" + getDistance( 1,2));
+}
